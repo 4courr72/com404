8d5f84a97a71e7c4932dd5b9f3b2d8c3ff614387
4courr72 <4courr72@solent.ac.uk>
2019-10-01 13:27:27 +0000

    1st IF statement

---
+++ 0-setup/1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,17 @@
+#First exercise of week 2
+print("What type of book is this?")
+book = input()
+#adbook = "adventure"
+print()
+if (book == "adventure"):
+    print("I like " + book + " books!")
+print()
+print ("Finished reading book.")
+#or I could write as:
+#print("What type of book is this?")
+#book = input()
+#print()
+#if (book = "adventure"):
+#    print("I like " + book + " books!")
+#print()
+#print ("Finished reading book.")
