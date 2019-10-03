234d9ccbbe25949163f99760cf90c02fc463291e
4courr72 <4courr72@solent.ac.uk>
2019-10-03 04:33:03 +0000

    Added code to demonstrate the use of multiple nested statements.

--- 0-setup/1-basics/3-decision/8-nestception/bot.py
+++ 0-setup/1-basics/3-decision/8-nestception/bot.py
@@ -7,19 +7,25 @@ where_to_look = input()
 if (where_to_look == "in the bedroom"):
     print("Where in the bedroom should I look?")
     where_in_bedroom = input()
-elif (where_in_bedroom == "under the bed"):
+    if (where_in_bedroom == "under the bed"):
         print("Found some shoes but no battery")
-
-#gets here if anything other than 'in the bedroom'
-#decide whether looking in bathroom or not - if yes, ask further question
+    else:
+        print("Found some mess but no battery.")
 else:
     if (where_to_look == "in the bathroom"):
         print("Where in the bathroom should I look?")
         where_in_bathroom = input()
-    elif (where_in_bathroom == "in the bathtub"):
-        print("Found a rubber duck but no battery")
+        if (where_in_bathroom == "in the bathtub"):
+            print("Found a rubber duck but no battery")
+        else:
+            print("It is wet but I found no battery.")
     else:
-        print("It is wet but I found no battery.")
-
-
-    print("Found some mess but no battery.")
+        if (where_to_look == "in the lab"):
+            print("Where in the lab should I look?")
+            where_in_lab = input()
+            if  (where_in_lab == "on the table"):
+                print("Yes! I found my battery")
+            else:
+                print("Found some tools but no battery.")
+        else:
+            print("I do not know where that is but I will keep looking.")
