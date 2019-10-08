c55b5306104173805537d225f5429be5161a538d
4courr72 <4courr72@solent.ac.uk>
2019-10-08 09:16:31 +0000

    Added code to demonstrate the use of an if statement. + post answer comments

--- 0-setup/1-basics/3-decision/1-if/bot.py
+++ 0-setup/1-basics/3-decision/1-if/bot.py
@@ -15,3 +15,4 @@ print ("Finished reading book.")
 #    print("I like " + book + " books!")
 #print()
 #print ("Finished reading book.")
+#Commment from 08-0-10-19: The commented section above is how Prins approached this in the answers posted later in the week. Using only one variable would be more memory efficient and easier to track. Noted.
