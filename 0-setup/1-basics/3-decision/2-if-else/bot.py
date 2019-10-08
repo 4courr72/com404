381d9d5bfc223dc1a4ea779dfef259cfa67fa3c5
4courr72 <4courr72@solent.ac.uk>
2019-10-08 09:33:00 +0000

    Added code to demonstrate the use of an if...else statement. + post answer comments

--- 0-setup/1-basics/3-decision/2-if-else/bot.py
+++ 0-setup/1-basics/3-decision/2-if-else/bot.py
@@ -7,4 +7,18 @@ if (activity == "calculate"):
 else:
     print("Performing activity...")
 print("Activity completed!")
+#Comment from reviwing answers 08-10-19: Although my code is fine, I am very light on comments. I have copied the answer below and hashed out to illustrate this:
+## Ask user for the type of activity
+#print("Please enter the activity to be performed.")
+#activity = input() 
+#
+## Determine if the activity is calculate
+#if (activity == "calculate"):
+#    print("\nPerforming calculations...")
+#else:
+#    print("\nPerforming activity...")
+#
+## Display message
+#print("\nActivity completed.")
 
+#[Comments 08-10-19 continued: I note the line space between elements - much clearer than mine using this.]
