60d6ca9865965c862ccbf22cb8b4802d951e9954
4courr72 <4courr72@solent.ac.uk>
2019-10-08 13:28:22 +0000

    Added code to demondstarte the use of a simple while loop.

---
+++ 0-setup/1-basics/4-repetition/1-while-loop/1-simple/bot.py
@@ -0,0 +1,14 @@
+#Asks user how many cables to remove
+print("How many cables should I remove?")
+
+#At stores the answer in the variable 'cables'
+cables = int(input())
+
+#Set a count variable ('removed_cables') to zero
+removed_cables = 0
+
+#While loop that functions whilst the number of cables removed is less than the total of cables to be removed
+while (removed_cables < cables):
+    removed_cables = removed_cables + 1 #Increments the cables_removed variable by one for each time round the loop
+    print("Removed cable.")
+
