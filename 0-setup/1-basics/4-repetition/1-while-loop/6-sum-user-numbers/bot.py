2c7694ddf913bba962ac1ba3c474e88e69f61947
4courr72 <4courr72@solent.ac.uk>
2019-10-08 15:02:17 +0000

    Added code to demonstrate the use of a while loop in performing calculations with user input

---
+++ 0-setup/1-basics/4-repetition/1-while-loop/6-sum-user-numbers/bot.py
@@ -0,0 +1,18 @@
+#Request user input for the quantity of numbers a user wishes to add together - store as a variable
+print("How many numbers should I add up?")
+quantity = int(input())
+
+#set a control variable as 0 - used to keep track of how many numbers we have asked for a number
+asked_for_so_far = 0
+
+#set a variable to store the total
+total = 0
+
+#A while loop to keep asking for a number the required number of times
+while asked_for_so_far < quantity:
+    asked_for_so_far = asked_for_so_far + 1
+    print("please enter number", asked_for_so_far, "of", quantity)
+    total = total + int(input())
+
+print("The answer is", total)
+
