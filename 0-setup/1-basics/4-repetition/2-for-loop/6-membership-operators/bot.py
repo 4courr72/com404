85a24c6f458498874766bc7fa1b43d01a398bbc2
4courr72 <4courr72@solent.ac.uk>
2019-10-09 20:43:53 +0000

    Not solved reverse printing using membership operators yet

--- 0-setup/1-basics/4-repetition/2-for-loop/6-membership-operators/bot.py
+++ 0-setup/1-basics/4-repetition/2-for-loop/6-membership-operators/bot.py
@@ -10,3 +10,15 @@ print("The phrase is: ",end="")
 
 #Completely stumped how to do this other than how I did it in previuos exercise
 #Will perserver or look at answers if no success
+#Had a cup of tea, a tab and 20 mins watching the telly......
+#[My trying continues...]Starting with a simple statement using the 'in' membership operator to just print out the phrase as is....
+#for x in phrase:
+#    print(x,end="")
+#print()
+#...which works well. Now, how do I reverse the order......????????? [and I know it is looping coz if no end="", then I get each letter on a new line]
+#'print(-x)' doesn't work - tried that (get an error).
+
+#I'm still missing something. The example had it in brackets...
+for (x in phrase):
+    print(x)
+#Mmmm, doesnt work at all. Time for bed me thinks
