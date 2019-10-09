385f34bf52aef5a25427ce67b97391d6557377aa
4courr72 <4courr72@solent.ac.uk>
2019-10-09 20:22:13 +0000

    Not solved reverse prinitng using membership operators yet

---
+++ 0-setup/1-basics/4-repetition/2-for-loop/6-membership-operators/bot.py
@@ -0,0 +1,12 @@
+#Ask the user for the phrase and save it in a variable 'phrase'
+print("What phrase do you see?")
+phrase = input()
+
+#Then display 'Reversing...' with an empty line below and above
+print("\nReversing...\n")
+
+#The start the line of the reversed test, display 'The phrase is' and suppress the lf/cr
+print("The phrase is: ",end="")
+
+#Completely stumped how to do this other than how I did it in previuos exercise
+#Will perserver or look at answers if no success
