e6be6e59bd05a05714a14d4798d5eb01dc6dbe3f
4courr72 <4courr72@solent.ac.uk>
2019-10-15 11:51:17 +0000

    Added code to demonstrate the use of a simple while loop.

--- 0-setup/1-basics/4-repetition/1-while-loop/2-count/bot.py
+++ 0-setup/1-basics/4-repetition/1-while-loop/2-count/bot.py
@@ -9,3 +9,4 @@ while (cables_avoided < max_live_cables):
     print("Avoiding...", end="")
     cables_avoided = cables_avoided + 1
     print("Done!", cables_avoided, "cables avoided.")
+#Comment 15/10/19: Answers published - same solution
