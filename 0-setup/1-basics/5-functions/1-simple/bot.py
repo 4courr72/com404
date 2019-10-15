51bb77cdeeffd894e06086d2216c5ce5a83c6917
4courr72 <4courr72@solent.ac.uk>
2019-10-15 13:27:01 +0000

    Added code to demonstrate the use of a user-defined function.

---
+++ 0-setup/1-basics/5-functions/1-simple/bot.py
@@ -0,0 +1,8 @@
+#Define a function for asking for a sound - call this function'listen'
+def listen():
+    print("What sound did I hear?")
+    sound = input()
+    print("\nThat was a loud", sound + "!")
+
+#Call the function
+listen()
