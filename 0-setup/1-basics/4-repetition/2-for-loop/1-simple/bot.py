886ca0198eee93389749415bbdc7713d31dafdec
4courr72 <4courr72@solent.ac.uk>
2019-10-15 12:12:00 +0000

    Added code to demonstrate the use of a simple for loop.

--- 0-setup/1-basics/4-repetition/2-for-loop/1-simple/bot.py
+++ 0-setup/1-basics/4-repetition/2-for-loop/1-simple/bot.py
@@ -9,10 +9,12 @@ print("Displaying...")
 #Then our for loop to display the required number of mountains
 for count in range (no_of_mountains):
     print("           __")
-    print("          /  \_")  
+    print("          /  \_")
     print("         /^    \ ")
     print("        /  ^    \_ ")
     print("      _/ ^ ^     ^\ ")
     print("     /  ^     ^    \ ")
 print()
 print("Done!")
+#Comment 15/10/19: Answers published - similar but Prins used the triple "
+#makes code much cleaner and then I don't need my print statement at the front of each row for the ascii art!"
