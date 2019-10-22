704ced51ae7656b8e2c10b37709ea90d370fdc95
4courr72 <4courr72@solent.ac.uk>
2019-10-22 14:03:39 +0000

    Answer - completed

---
+++ 0-setup/1-basics/TCAs/AE1-Review-TCA2[7-Qs]/Q1.py
@@ -0,0 +1,6 @@
+#Ask for users answer to a question and store response in a variable
+print("What happens when the last petal falls?")
+what_happens = (input())
+
+#Output a message incorporating the user's response
+print("My dear Bella when the last petal falls", what_happens)
