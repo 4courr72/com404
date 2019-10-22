7e97681d45fe22e504ba30e282646a396f5e4b43
4courr72 <4courr72@solent.ac.uk>
2019-10-22 14:10:32 +0000

    Answer - completed

---
+++ 0-setup/1-basics/TCAs/AE1-Review-TCA2[7-Qs]/Q2.py
@@ -0,0 +1,10 @@
+#Ask user for a resonse and store in a variable
+print("Have you fear in your heart?")
+fear_in_heart = input()
+
+#Output various statements depending on user's response
+if (fear_in_heart == "yes"):
+    print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
+else:
+    print("The force is strong in you. You may be a Jedi apprentice.")
+
