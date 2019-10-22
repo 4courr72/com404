3492adac5a87e2b5bbbfaff39243718fdf18c6b7
4courr72 <4courr72@solent.ac.uk>
2019-10-22 13:11:51 +0000

    Answer - complete

---
+++ 0-setup/1-basics/TCAs/AE1 Review - TCA1/bot.py
@@ -0,0 +1,6 @@
+#Asking for users name and assigning to a variable
+print("By what name are you known?")
+name = input()
+
+#Outputting a response incorporating the user's name
+print("Live long and prosper", name)
