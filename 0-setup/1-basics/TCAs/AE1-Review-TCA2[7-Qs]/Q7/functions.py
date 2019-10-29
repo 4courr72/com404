e960b283c2a856feb6dee14ee0350c14f6b575b6
4courr72 <4courr72@solent.ac.uk>
2019-10-29 13:04:59 +0000

    Work in progress

---
+++ 0-setup/1-basics/TCAs/AE1-Review-TCA2[7-Qs]/Q7/functions.py
@@ -0,0 +1,29 @@
+#Define the modeules for use in the main program:
+
+#Define line under module
+def line_under(word):
+    print(word)
+    print("*" * (len(word)))
+
+#Define line over module
+def line_over(word):
+    print("*" * (len(word)))
+    print(word)
+
+#define line both over and under module
+def line_both(word):
+    print("*" * (len(word)))
+    print(word)
+    print("*" * (len(word)))
+
+#Define grid module
+def grid(word, rows, columns):
+    printed_columns = columns
+    if printed_columns > 0:
+        print("*" * len(word), "   ", end = "")
+        printed_columns = printed_columns - 1
+    printed_columns = printed_columns
+    if printed_columns > 0:
+        print("*" * len(word), "   ")
+        printed_columns = printed_columns - 1
+
