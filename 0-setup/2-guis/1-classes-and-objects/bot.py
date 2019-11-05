d99921c14acc3e8b775533be1a0c48ff3db326bf
4courr72 <4courr72@solent.ac.uk>
2019-11-05 15:12:11 +0000

    Work in progress

--- 0-setup/2-guis/1-classes-and-objects/bot.py
+++ 0-setup/2-guis/1-classes-and-objects/bot.py
@@ -7,9 +7,47 @@ class bot:
         self.shield_level = shield_level
 
     def display_name(self):
-        print("*" * len(name))
-        print("*" + name + "*")
-        print("*" * len(name))
+        print("*" * len(self.name) + "**")
+        print("*" + self.name + "*")
+        print("*" * len(self.name) + "**")
 
-bot1 = bot(Ross, 53, 10, 20)
-display_name(bot1)
+    def display_age(self):
+        print(self.age)
+        print("     $  $")       
+        print("     |  |")
+        print("   ********")
+        print("  **********")
+        print(" {  You are }")
+        print(" {   ", self.age, "   }")
+        print("  **********")
+        print("    I    I")
+        print("----------------")
+
+    def display_energy(self):
+        print("█" * self.energy)
+
+    def display_shield(self):
+        print("__________")
+        print("| Shield:|")
+        print("|   ", self.shield_level, " |")
+        print(" \      /")
+        print("  \    /")
+        print("   \  /")
+        print("    VV")
+
+    def display_summary(self):
+        bot1.display_name()
+        bot1.display_energy()
+        bot1.display_shield()
+
+    def __str__(self):
+        return("name="+self.name, "age="+str(self.age))
+
+
+bot1 = bot("Ross", 53, 10, 20)
+#bot1.display_name()
+#bot1.display_age()
+#bot1.display_energy()
+#bot1.display_shield()
+#bot1.display_summary()
+print(bot1)
