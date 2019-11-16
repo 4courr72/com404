59dfca902b722018ab2eeb5b6ec9c8b975f07158
Ross Courtney <4courr72@solent.ac.uk>
2019-11-16 14:46:15 +0000

    Work in progress for trying pack

--- 0-setup/2-guis/2-windows-layout/2-pack/gui.py
+++ 0-setup/2-guis/2-windows-layout/2-pack/gui.py
@@ -14,22 +14,24 @@ class Gui(Tk):
 
 
         # add window components
+        
         self.__add_heading_label()
         self.__add_message_label()
-        self.__add_a_button()
+        self.__add_email_frame()
         self.__add_box_label()
         self.__add_inner_frame()
-        self.__add_email_frame()
+        self.__add_a_button()
         
     def __add_email_frame(self):
-        self.email_frame = Frame()
+        self.email_frame = Frame(width=20, height=2)
         self.email_frame.pack()
+        # self.email_frame.configure(width = 40)
 
     def __add_heading_label(self): #the 2 leading underscores is to make it private!
         #Remember these three steps:
         # create component
-        self.heading_label = Label(self.email_frame) #'label' is a class already in TKinter
-        self.heading_label.pack() # 'place' is .... 0.0 is top left corner and gets bigger as you go down
+        self.heading_label = Label() #'label' is a class already in TKinter
+        self.heading_label.pack(side=LEFT) # 'place' is .... 0.0 is top left corner and gets bigger as you go down
         # style component
         #'configure' allows you to set multiple attributes in one command, seperated by commas. put on new line to make easier to read
         self.heading_label.configure(   font = "Arial 18",
@@ -46,13 +48,13 @@ class Gui(Tk):
 
     def __add_box_label(self):
         self.box_label = Label()
-        self.box_label.pack(side=LEFT)
+        self.box_label.pack() #Took this out of brackets: self.email_frame, side=LEFT
         self.box_label.configure(   font = "Arial 10",
                                     text="Email:")
 
     def __add_a_button(self):
         self.a_button = Button()
-        self.a_button.pack()
+        self.a_button.pack(self.email_frame)
         self.a_button.configure(    text = "Subscribe",
                                     font = "Arial 8",
                                     underline = 0,
@@ -61,7 +63,7 @@ class Gui(Tk):
     
     def __add_inner_frame(self):
         self.inner_frame = Entry()
-        self.inner_frame.pack()
+        self.inner_frame.pack(self.email_frame)
         self.inner_frame.configure( font = "Arial 10",
                                     width = 40)
         
