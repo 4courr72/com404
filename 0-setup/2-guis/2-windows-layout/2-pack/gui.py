57980a0aba8d1a9043a334d71df9261a34c0918f
Ross Courtney <4courr72@solent.ac.uk>
2019-11-16 12:28:47 +0000

    Work in progress for trying pack

--- 0-setup/2-guis/2-windows-layout/2-pack/gui.py
+++ 0-setup/2-guis/2-windows-layout/2-pack/gui.py
@@ -19,12 +19,16 @@ class Gui(Tk):
         self.__add_a_button()
         self.__add_box_label()
         self.__add_inner_frame()
+        self.__add_email_frame()
+        
+    def __add_email_frame(self):
+        self.email_frame = Frame()
+        self.email_frame.pack()
 
     def __add_heading_label(self): #the 2 leading underscores is to make it private!
-        
         #Remember these three steps:
         # create component
-        self.heading_label = Label() #'label' is a class already in TKinter
+        self.heading_label = Label(self.email_frame) #'label' is a class already in TKinter
         self.heading_label.pack() # 'place' is .... 0.0 is top left corner and gets bigger as you go down
         # style component
         #'configure' allows you to set multiple attributes in one command, seperated by commas. put on new line to make easier to read
@@ -32,31 +36,32 @@ class Gui(Tk):
                                         text="RECEIVE OUR NEWSLETTER")
         # add component events
 
+
     def __add_message_label(self):
         #pass
         self.message_label = Label()
-        self.message_label.place(x = 170, y = 80)
+        self.message_label.pack()
         self.message_label.configure(   font = "Arial 9", 
                                         text="Please enter your email below to receive our newsletter.")
 
+    def __add_box_label(self):
+        self.box_label = Label()
+        self.box_label.pack(side=LEFT)
+        self.box_label.configure(   font = "Arial 10",
+                                    text="Email:")
 
     def __add_a_button(self):
         self.a_button = Button()
-        self.a_button.place(x = 10, y = 170)
+        self.a_button.pack()
         self.a_button.configure(    text = "Subscribe",
                                     font = "Arial 8",
                                     underline = 0,
                                     justify = CENTER,
                                     width = 62)
-
-    def __add_box_label(self):
-        self.box_label = Label()
-        self.box_label.place(x = 20, y = 130)
-        self.box_label.configure(   font = "Arial 10",
-                                    text="Email:")
-
+    
     def __add_inner_frame(self):
         self.inner_frame = Entry()
-        self.inner_frame.place(x = 80, y = 130)
+        self.inner_frame.pack()
         self.inner_frame.configure( font = "Arial 10",
                                     width = 40)
+        
