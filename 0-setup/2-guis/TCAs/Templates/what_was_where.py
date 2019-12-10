a03796faaade33e52c5fee7e9f51204d03b3876d
Ross Courtney <4courr72@solent.ac.uk>
2019-12-10 09:17:19 +0000

    WIP

--- 0-setup/2-guis/TCAs/Templates/what_was_where.py
+++ 0-setup/2-guis/TCAs/Templates/what_was_where.py
@@ -1,2 +1,26 @@
-#Crosses and ticks - 
-#? - 4-hiding-and-showing
+#Cross and ticks - 
+#Use of LEN - \2-guis\1-classes-and-objects\
+
+
+
+#Button configure options:
+#        self.a_button.configure(    text = "Subscribe",
+#                                    font = "Arial 8",
+#                                   underline = 0,
+#                                    justify = CENTER,
+#                                    width = 62)
+
+#Label configure options:
+#        self.box_label.configure(   font = "Arial 10",
+#                                    text="Email:")
+
+#Entry configure options:
+#        self.inner_frame.configure( font = "Arial 10",
+#                                    width = 40)
+
+#EVENTS:
+#self.buy_button.bind("<ButtonRelease-1>", self.__buy_button_clicked)[RC: 1=left button release, 2=middle, 3=right]
+#def __buy_button_clicked(self, event):
+
+#("<KeyRelease>"
+#
